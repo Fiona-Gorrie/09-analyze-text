9a6fc0eb2e50b20d8035b8fabdc525129a5305d6
Fiona-Gorrie <47618889+Fiona-Gorrie@users.noreply.github.com>
2019-04-24 22:01:46 -0600

    Update analyze_text.py

--- analyze_text.py
+++ analyze_text.py
@@ -1,4 +1,23 @@
-# Remember to reach out for help after your own due diligence
+def analyze_text(string):
+    ecount=0
+    for i in string: 
+        if i == "e": 
+            ecount+=1
 
-def analyze_text(text):
-    # Your code here
+    capital_e_count=0
+    for E in string: 
+        if E == "E": 
+            capital_e_count+=1
+
+    total_e = capital_e_count + ecount        
+        
+    count=0
+    for a in string: 
+        if (a.isalpha()) == True: 
+                count+=1
+    percent = 100 * total_e / len(string)
+    str_percent = "{:.2f}".format(percent)
+                 
+    return "the text contains " + str(count) + " alphabetic characters, of which "+str(total_e) + " (" + str_percent + "%) of them are e"
+
+print(analyze_text("feEling"))
